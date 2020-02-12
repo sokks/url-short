d183bfcaf58caa1218dce8c32fb85a0b08d7923e
Nikita Voynov <voynov@nikscorp.com>
2020-02-13 01:00:24 +0300

    disable some linters as wont fix

--- hasher/hasher.go
+++ hasher/hasher.go
@@ -1,7 +1,7 @@
 package hasher
 
 import (
-	"crypto/sha1"
+	"crypto/sha1" //nolint:gosec
 	"strconv"
 
 	"github.com/lytics/base62"
@@ -9,7 +9,7 @@ import (
 
 func HashURL(url string, miniSalt uint64) string {
 	data := []byte(url + strconv.FormatUint(miniSalt, 10))
-	sha1sum := sha1.Sum(data)
+	sha1sum := sha1.Sum(data) //nolint:gosec
 	var hash = make([]byte, 100)
 	base62.StdEncoding.Encode(hash, sha1sum[:])
 	// log.Printf("hash: %+v", hash)
